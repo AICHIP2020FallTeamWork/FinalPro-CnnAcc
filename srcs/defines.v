761c74c0df58e7c5690396da7b45e3d86393fe2d
thusjr <songdundunn@163.com>
2020-12-01 19:29:22 +0800

    add files define

---
+++ srcs/defines.v
@@ -0,0 +1,22 @@
+`define RstEnable 1'b0
+`define RstDisable 1'b1
+
+`define ZeroWord 32'h0
+
+`define Layer1 3'd1
+`define Layer2 3'd2
+`define Layer3 3'd3
+`define Layer4 3'd4
+`define Layer5 3'd5
+
+
+`define PeBufSize   31:0
+`define Byte        7:0
+`define EightByte   63:0
+
+
+
+
+
+`define True 1'b1
+`define False 1'b0
