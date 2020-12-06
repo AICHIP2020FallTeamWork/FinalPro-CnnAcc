c229f3a5905eca4a51615b8ed3343117dc3e0156
thusjr <songdundunn@163.com>
2020-12-06 18:55:44 +0800

    weightRom finished

--- srcs/top.v
+++ srcs/top.v
@@ -21,6 +21,19 @@ clk_wiz_0 clk0(
 //-------------------------------------------
 
 
+wire [16:0] addr_weight_1;
+wire [16:0] addr_weight_2;
+wire [7:0] dout_weight_1;
+wire [7:0] dout_weight_2;
+
+weightROM weightRom(
+    .addra(addr_weight_1),
+    .addrb(addr_weight_2),
+    .clka(clk),
+    .clkb(clk),
+    .douta(dout_weight_1),
+    .doutb(dout_weight_2)
+);
 
 
 //-------------------------------------------
