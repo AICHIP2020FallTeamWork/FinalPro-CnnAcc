6422efd4368b13c7700ce93704d034c360889f3b
thusjr <songdundunn@163.com>
2020-12-06 21:06:19 +0800

    BRAM4k,BRAM32k,finished in top module

--- srcs/top.v
+++ srcs/top.v
@@ -33,16 +33,45 @@ weightROM weightRom(
     .doutb(dout_weight_2)
 );
 //-------------------------------------------
-
-
-
-
+reg we_BRAM32k;
+wire [11:0] addr_BRAM32k_1;
+wire [11:0] addr_BRAM32k_2;
+wire [63:0] din_BRAM32k_1;
+wire [63:0] din_BRAM32k_2;
+wire [63:0] dout_BRAM32k_1;
+wire [63:0] dout_BRAM32k_2;
+BRAM32k bram32k(
+    .addra(addr_BRAM32k_1),
+    .addrb(addr_BRAM32k_2),
+    .clka(clk),
+    .clkb(clk),
+    .dina(din_BRAM32k_1),
+    .dinb(din_BRAM32k_2),
+    .douta(dout_BRAM32k_1),
+    .doutb(dout_BRAM32k_2),
+    .wea(we_BRAM32k),
+    .web(we_BRAM32k)
+);
 //-------------------------------------------
-
-
-
-
-
+reg we_BRAM4k;
+wire [11:0] addr_BRAM4k_1;
+wire [11:0] addr_BRAM4k_2;
+wire [63:0] din_BRAM4k_1;
+wire [63:0] din_BRAM4k_2;
+wire [63:0] dout_BRAM4k_1;
+wire [63:0] dout_BRAM4k_2;
+BRAM4k bram4k(
+    .addra(addr_BRAM4k_1),
+    .addrb(addr_BRAM4k_2),
+    .clka(clk),
+    .clkb(clk),
+    .dina(din_BRAM4k_1),
+    .dinb(din_BRAM4k_2),
+    .douta(dout_BRAM4k_1),
+    .doutb(dout_BRAM4k_2),
+    .wea(we_BRAM4k),
+    .web(we_BRAM4k)
+);
 //-------------------------------------------//--------
 
 
